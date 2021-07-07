16d1e99caeacae078113e71669cbe49f72da8eaf
GabezRules <gabriela.zofia.zawadzka@gmail.com>
2021-07-07 20:41:59 +0200

    Add wrong location assert

--- app/src/main/java/com/gabez/nearesttoiletpl/ui/MainActivity.kt
+++ app/src/main/java/com/gabez/nearesttoiletpl/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.gabez.nearesttoiletpl
+package com.gabez.nearesttoiletpl.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gabez.nearesttoiletpl.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/gabez/nearesttoiletpl/ui/fragments/wrong_location/WrongLocationViewModel.kt
@@ -0,0 +1,6 @@
+package com.gabez.nearesttoiletpl.ui.fragments.wrong_location
+
+import androidx.lifecycle.ViewModel
+
+class WrongLocationViewModel: ViewModel() {
+}

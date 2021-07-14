8bd11db0818e8dba2424249a7be1eab5e5caaae1
GabezRules <gabriela.zofia.zawadzka@gmail.com>
2021-07-14 13:01:06 +0200

    Show no internet dialog

--- app/src/main/java/com/gabez/nearesttoiletpl/receivers/NetworkStatusChangedReceiver.kt
+++ app/src/main/java/com/gabez/nearesttoiletpl/receivers/NetworkStatusChangedReceiver.kt
@@ -4,15 +4,19 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.gabez.nearesttoiletpl.NetworkUtil
+import com.gabez.nearesttoiletpl.ui.CurrentActivityUtil
+import com.gabez.nearesttoiletpl.ui.StartActivity
 
 class NetworkStatusChangedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val status: Int = NetworkUtil.getConnectivityStatusString(context)
         if ("android.net.conn.CONNECTIVITY_CHANGE" == intent.action) {
             if (status == NetworkUtil.NETWORK_STATUS_NOT_CONNECTED) {
-                //no internet
+                if (CurrentActivityUtil.currentActivityClassName == StartActivity.javaClass.name)
+                    StartActivity.showNoInternetDialog()
             } else {
-                //we have internet!
+                if (CurrentActivityUtil.currentActivityClassName == StartActivity.javaClass.name)
+                    StartActivity.hideNoInternetDialog()
             }
         }
     }

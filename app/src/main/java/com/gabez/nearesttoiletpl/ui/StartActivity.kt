00b03e1c40fa8a97e089affc46ef549b68e6a14c
GabezRules <gabriela.zofia.zawadzka@gmail.com>
2021-07-14 13:01:06 +0200

    Show no internet dialog

--- app/src/main/java/com/gabez/nearesttoiletpl/ui/StartActivity.kt
+++ app/src/main/java/com/gabez/nearesttoiletpl/ui/StartActivity.kt
@@ -1,10 +1,12 @@
 package com.gabez.nearesttoiletpl.ui
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
+import com.gabez.nearesttoiletpl.NetworkUtil
 import com.gabez.nearesttoiletpl.R
 
 class StartActivity : AppCompatActivity() {
@@ -13,9 +15,47 @@ class StartActivity : AppCompatActivity() {
         setContentView(R.layout.activity_start)
 
         startActivityContext = this
+        CurrentActivityUtil.currentActivityClassName = this.javaClass.name
+
+        if (NetworkUtil.getConnectivityStatus(this) === 0) {
+            showNoInternetDialog()
+        }
+
+        val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager?.let {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                it.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
+                    override fun onAvailable(network: Network) {
+                        hideNoInternetDialog()
+                    }
+                    override fun onLost(network: Network) {
+                        showNoInternetDialog()
+                    }
+                })
+            }
+        }
     }
 
     companion object{
         lateinit var startActivityContext: Context
+        var noInternetDialog: AlertDialog? = null
+
+        fun showNoInternetDialog() {
+            if (startActivityContext != null) {
+                noInternetDialog =
+                    AlertDialog.Builder(startActivityContext).create()
+                noInternetDialog!!.setTitle(startActivityContext.resources.getString(R.string.service_unavailable))
+                noInternetDialog!!.setMessage(startActivityContext.resources.getString(R.string.no_internet_alert))
+                noInternetDialog!!.setCancelable(false)
+                noInternetDialog!!.show()
+            }
+        }
+
+        fun hideNoInternetDialog() {
+            if (noInternetDialog != null) {
+                noInternetDialog!!.hide()
+                noInternetDialog = null
+            }
+        }
     }
 }

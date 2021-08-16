99ea136e60a7b450627f64fe8c3a9b340315d510
GabezRules <gabriela.zofia.zawadzka@gmail.com>
2021-08-16 22:19:08 +0200

    NTP-13 Show toilet details

---
+++ app/src/main/java/com/gabez/nearesttoiletpl/ui/fragments/toilet_details/ToiletDetailsFragment.kt
@@ -0,0 +1,42 @@
+package com.gabez.nearesttoiletpl.ui.fragments.toilet_details
+
+import android.content.Context
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RatingBar
+import android.widget.TextView
+import com.gabez.nearesttoiletpl.R
+import com.gabez.nearesttoiletpl.domain.entity.Toilet
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
+
+class ToiletDetailsFragment(val toilet: Toilet, val context: Context) {
+    private var bottomSheetDialog: BottomSheetDialog
+
+    init {
+        bottomSheetDialog = BottomSheetDialog(context)
+    }
+
+    fun showBottomSheetDialog(){
+        bottomSheetDialog.setContentView(R.layout.fragment_toilet_details)
+
+        val title = bottomSheetDialog.findViewById<TextView>(R.id.toiletName)!!
+        val rateCount = bottomSheetDialog.findViewById<TextView>(R.id.rateCount)!!
+        val ratingBar = bottomSheetDialog.findViewById<RatingBar>(R.id.ratingBar)!!
+        val navigateBtn = bottomSheetDialog.findViewById<LinearLayout>(R.id.navigateBtn)!!
+
+        bottomSheetDialog.show()
+
+        navigateBtn.setOnClickListener { hideBottomSheetDialog() }
+
+        val titleString: String = toilet.displayName.split(", ").subList(0, 3).joinToString(", ")
+
+        rateCount.text = "(${toilet.rateCount})"
+        ratingBar.rating = toilet.rate
+        title.text = titleString
+    }
+
+    fun hideBottomSheetDialog(){
+        bottomSheetDialog.hide()
+    }
+}
